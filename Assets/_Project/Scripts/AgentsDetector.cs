c90237a085ad4aff2a5b822cf2c854a5d93de895
AliSaidi-Stolenpad <ali.saidi@stolenpad-studio.com>
2022-06-09 20:04:02 +0100

    DoTween

---
+++ Assets/_Project/Scripts/AgentsDetector.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AgentsDetector : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FrostEnemy") || other.CompareTag("FireEnemy"))
+        {
+            EnemyController EC = other.GetComponent<EnemyController>();
+            Transform t1 = EC.transform.parent;
+            Transform t2 = EC.transform;
+
+            EC.splineFollower.enabled = false;
+
+            t2.SetParent(null);
+            t1.transform.position = t2.transform.position;
+            if (other.CompareTag("FireEnemy"))
+            {
+                t1.LookAt(transform);
+            }
+                
+            t2.SetParent(t1);
+            
+            EC.player = transform;
+        }
+    }
+}
