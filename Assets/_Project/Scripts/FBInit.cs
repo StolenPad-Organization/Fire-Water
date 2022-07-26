049f5cd19b2092577cd6d07e719f4e2e78b29b4e
AliSaidi-Stolenpad <ali.saidi@stolenpad-studio.com>
2022-07-26 16:11:25 +0100

    SDK

---
+++ Assets/_Project/Scripts/FBInit.cs
@@ -0,0 +1,12 @@
+using Facebook.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBInit : MonoBehaviour
+{
+    void Start()
+    {
+        FB.Init("438932148137328");
+    }
+}
